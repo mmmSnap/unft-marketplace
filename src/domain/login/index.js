8f5c44a3ba638c7598e0232cee0dbc9f1ceb4ec2
mojahid <sahparanaaz@Mds-MacBook-Air.local>
2023-09-03 23:13:26 +0530

    feat: added color and icon for the button

--- src/domain/login/index.js
+++ src/domain/login/index.js
@@ -3,9 +3,10 @@ import RadioButton from '../../components/RadioButton/RadioButton';
 import Typography from '../../components/Typography/Typography';
 import BlockButton from '../../components/BlockButton/BlockButton';
 import InputElement from '../../components/InputElement/InputElement';
+import IconGooglePlus from '../../svgIcons/IconGooglePlus';
 import Hr from '../../components/hr/Hr'
 import style from './index.module.sass'
-import {LoginTypeRadioOption}  from './const'
+import { LoginTypeRadioOption } from './const'
 
 const LoginComponent = () => {
     const [selectedOption, setSelectedOption] = React.useState('Explorer Login');
@@ -17,41 +18,45 @@ const LoginComponent = () => {
     return (
         <div className={style.container}>
             <div className={style.childDiv}>
-                <div style={{margin:"10px 0px"}}>
-                <Typography component="h1" variant="h5" >
-                   Sign in or Sign up
-                </Typography>
-                </div>
-             
-                <div className={style.radioInline}>
-                    {LoginTypeRadioOption.map((items)=>{
-                        return (
-                            <RadioButton
-                            key={items.id}
-                            label={items.name}
-                            checked={selectedOption ===items.name}
-                            onChange={handleOptionChange}
-                        />
-                        )
-                    })}
+                <div>
                    
                 </div>
-                <div style={{width:"80%",margin:"10px 0px"}}>
-                <BlockButton   varient='outlined' >
-                    Contine with google
-                </BlockButton>
-               </div>
-               <Hr>
-                  OR
-               </Hr>
-               <div style={{width:"80%",margin:"20px 0px"}}>
-                <InputElement  />
-               </div>
-               <div style={{width:"80%",margin:"20px 0px"}}>
-                <BlockButton    >
-                    Submit
-                </BlockButton>
-               </div>
+                <div style={{ margin: "10px 0px" }}>
+                    <Typography component="h1" variant="h5" >
+                        Sign in or Sign up
+                    </Typography>
+                </div>
+
+                <div className={style.radioInline}>
+                    {LoginTypeRadioOption.map((items) => {
+                        return (
+                            <RadioButton
+                                key={items.id}
+                                label={items.name}
+                                checked={selectedOption === items.name}
+                                onChange={handleOptionChange}
+                            />
+                        )
+                    })}
+
+                </div>
+                <div style={{ width: "80%", margin: "10px 0px" }}>
+                
+                    <BlockButton varient='outlined' >
+                     <IconGooglePlus  />Contine with google
+                    </BlockButton>
+                </div>
+                <Hr>
+                    OR
+                </Hr>
+                <div style={{ width: "80%", margin: "20px 0px" }}>
+                    <InputElement />
+                </div>
+                <div style={{ width: "80%", margin: "20px 0px" }}>
+                    <BlockButton    >
+                        Submit
+                    </BlockButton>
+                </div>
             </div>
         </div>
     )
--- src/pages/auth/login.js
+++ src/pages/auth/login.js
@@ -1,8 +1,10 @@
 import React from 'react'
-import LoginComponent from '../../domain/login/index'
-import RadioButton from '../../components/RadioButton/RadioButton'
+import { getDataByCategory, getAllDataByType } from '../../lib/cosmic'
 
-const LoginPage = () => {
+import LoginComponent from '../../domain/login/index'
+import Layout from '../../components/Layout';
+
+const LoginPage = ({navigationItems}) => {
 
     const [selectedOption, setSelectedOption] = React.useState('Option 1');
 
@@ -11,8 +13,19 @@ const LoginPage = () => {
     };
 
     return (
-       <LoginComponent />
+        <Layout navigationPaths={navigationItems[0]?.metadata}>
+            <LoginComponent />
+        </Layout>
     )
+}
+
+export async function getServerSideProps() {
+    const navigationItems = (await getAllDataByType('navigation')) || []
+    const landing = (await getAllDataByType('landings')) || []
+
+    return {
+        props: { navigationItems, landing },
+    }
 }
 
 export default LoginPage
